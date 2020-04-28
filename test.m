3b47a8cd283b028b7cf1acd89d50281e7c9d2276
antoineD136 <antoine.del99@gmail.com>
2020-04-28 22:03:51 +0200

    t

--- test.m
+++
@@ -1,6 +0,0 @@
-function [temps] = test(rayon, positionCentre, positionCote, vitesse)
-T_1 = positionCote - positionCentre ./ vitesse;
-f = @positionCentre + rayon - positionCote + vitesse.*x;
-temps = fzero(@f, T_1);
-end
-
